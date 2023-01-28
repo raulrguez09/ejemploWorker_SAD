1c887a1e073f1b97d430b8ffcb0de10aa0eaab5e
raulrguez09 <44619080+raulrguez09@users.noreply.github.com>
2023-01-28 21:04:07 +0100

    create factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,6 @@
 import math
 import sys
 
+# Ejemplo de programa que suma el factorial de dos numeros pasados por parametro
 res = math.factorial(int(sys.argv[1])) + math.factorial(int(sys.argv[2])) 
 print("La suma de " + sys.argv[1] + "! + " + sys.argv[2] + "! = " + str(res))
