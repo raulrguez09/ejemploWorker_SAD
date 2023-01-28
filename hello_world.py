594103d5732195934f34f621fa5eae3631da02ec
raulrguez09 <44619080+raulrguez09@users.noreply.github.com>
2023-01-28 21:01:36 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,5 @@
-print('Hello, world!')
+import math
+import sys
+
+res = math.factorial(int(sys.argv[1])) + math.factorial(int(sys.argv[2])) 
+print("La suma de " + sys.argv[1] + "! + " + sys.argv[2] + "! = " + str(res))
